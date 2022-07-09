831e1de8975bfce5ec7816d21c59ef0eaa598fd7
r0guelands <mrcelogranzotto@hotmail.com.br>
2022-07-08 23:35:45 -0300

    adding searchbox

--- public/scripts/sort.js
+++ public/scripts/sort.js
@@ -3,33 +3,55 @@ function sortTableByColumn(table, column, asc = true) {
     const tBody = table.tBodies[0];
     const rows = Array.from(tBody.querySelectorAll("tr"));
     const sortedRows = rows.sort((a, b) => {
-      const aCol = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-      const bCol = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-      return aCol > bCol ? (1 * dirModifier) : -(1 * dirModifier);
+        const aCol = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bCol = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        return aCol > bCol ? (1 * dirModifier) : -(1 * dirModifier);
     });
-  
+
     while (tBody.firstChild) {
-      tBody.removeChild(tBody.firstChild);
+        tBody.removeChild(tBody.firstChild);
     }
-  
+
     tBody.append(...sortedRows);
-  
+
     table.querySelectorAll("th").forEach(th => {
-      th.classList.remove("th-sort-asc", "th-sort-desc");
+        th.classList.remove("th-sort-asc", "th-sort-desc");
     });
-  
+
     table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
-  
+
     table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
-  
-  }
-  
-  document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+
+}
+
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
     headerCell.addEventListener("click", () => {
-      const tableElement = headerCell.parentElement.parentElement.parentElement;
-      const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-      const currentIsAscending = headerCell.classList.contains("th-sort-asc");
-  
-      sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
     });
-  });
+});
+
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
