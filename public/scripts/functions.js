62663326795a0c2dc67e77b2f72f060e9ac02650
r0guelands <mrcelogranzotto@hotmail.com.br>
2022-07-11 01:50:46 -0300

    corrected login page bug

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,15 +1,66 @@
 const togglePassword = document.getElementById("toggle-password");
 const password = document.getElementById("password");
 
-togglePassword.addEventListener("click", () => {
+if (document.getElementById("submit")) {
 
-    if (password.type === "password") {
+    togglePassword.addEventListener("click", () => {
+
+        if (password.type === "password") {
+            password.type = "text";
+        } else {
+            password.type = "password";
+        }
+
+    });
+
+    document.getElementById("randomTextGenerator").addEventListener("click", () => {
+
+        let chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+ABCDEFGHIJKLMNOP1234567890";
+        let pass = "";
+
+        for (let x = 0; x < 18; x++) {
+            let i = Math.floor(Math.random() * chars.length);
+            pass += chars.charAt(i);
+        }
+
+        if (togglePassword.checked == false) {
+            togglePassword.checked = !togglePassword.checked;
+        }
+
+        password.value = pass;
         password.type = "text";
-    } else {
-        password.type = "password";
-    }
 
-});
+    });
+
+    document.getElementById("submit").addEventListener("click", () => {
+
+        document.getElementById("userPass").value = sessionStorage.passwordLocal;
+
+    });
+
+}
+
+if (document.getElementById("loginPage")) {
+
+    document.getElementById("buttonSubmit").addEventListener("click", () => {
+        let input = document.getElementById("password");
+        let userPassword = input.value;
+        sessionStorage.passwordLocal = userPassword;
+    });
+
+
+
+    togglePassword.addEventListener("click", () => {
+
+        if (password.type === "password") {
+            password.type = "text";
+        } else {
+            password.type = "password";
+        }
+
+    });
+
+}
 
 if (document.getElementById("title")) {
 
@@ -19,32 +70,6 @@ if (document.getElementById("title")) {
     }
 
 }
-
-
-document.getElementById("randomTextGenerator").addEventListener("click", () => {
-
-    let chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+ABCDEFGHIJKLMNOP1234567890";
-    let pass = "";
-
-    for (let x = 0; x < 18; x++) {
-        let i = Math.floor(Math.random() * chars.length);
-        pass += chars.charAt(i);
-    }
-
-    if (togglePassword.checked == false) {
-        togglePassword.checked = !togglePassword.checked;
-    }
-
-    password.value = pass;
-    password.type = "text";
-
-});
-
-document.getElementById("submit").addEventListener("click", () => {
-
-    document.getElementById("userPass").value = sessionStorage.passwordLocal;
-
-});
 
 if (document.getElementById("date")) {
     const options = {
--- public/scripts/localpass.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("buttonSubmit").addEventListener("click", () => {
-    let input = document.getElementById("password");
-    let userPassword = input.value;
-    sessionStorage.passwordLocal = userPassword;
-});
